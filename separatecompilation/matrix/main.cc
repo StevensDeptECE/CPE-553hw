f598983e917be7af9b6888fc7f7b26ab122db4d2
Dov Kruger <Dov.Kruger@stevens.edu>
2021-12-10 13:09:34 -0500

    matrix with exception handling

--- separatecompilation/matrix/main.cc
+++ separatecompilation/matrix/main.cc
@@ -1,13 +1,20 @@
 #include "matrix.hh"
 
 int main() {
-	const matrix a(3,4, 2.5); // create a matrix with 3 rows, 4 columns each.
-	// fill each element with 2.5
 
-	matrix b = a; // copy matrix a
-	b(1,2) = 1.1; // set the element at row 1, column 2 = 1.1
+	try {
+		const matrix a(3,4, 2.5); // create a matrix with 3 rows, 4 columns each.
+		// fill each element with 2.5
 
-	cout << b << '\n';
-	matrix c = a + b;
-	cout << c << '\n';
+		matrix b = a; // copy matrix a
+		b(1,2) = 1.1; // set the element at row 1, column 2 = 1.1
+
+		cout << b << '\n';
+		matrix c = a + b;
+		cout << c << '\n';
+		matrix d(2,2, 1.9);
+		matrix e = c + d;
+	} catch(const bad_size& e) {
+		cerr << e << '\n';
+	}	
 }
